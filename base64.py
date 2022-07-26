e7d52f519044507b0173ed194c92b81b1652fc9b
saulomonteirolimeira <92337040+saulomonteirolimeira@users.noreply.github.com>
2022-07-26 15:16:10 -0300

    Update base64.py

--- base64.py
+++ base64.py
@@ -5,3 +5,14 @@ with open("logo.png", "rb") as image_file:
 print(base64)
 
 # b'iVBORw0KGgoAAAANS.....'
+
+'''
+#Tem programa que so aceita nesse formato de baixo
+with open("rone.png", "rb") as img_file:
+    b64_string = base64.b64encode(img_file.read())
+print(b64_string.decode('utf-8'))
+'''
+
+# iVBORw0KGgoAAAA
+
+
